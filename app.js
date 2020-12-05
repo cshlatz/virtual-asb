d6c4c481fdfbeaa7b4ceb3e71cdb8e7a6a0bcbbe
Connor Shlatz <cshlatz@gmail.com>
2020-12-05 14:14:50 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const papa = require('papaparse');
+
+const file = fs.createReadStream('games.csv');
+
+var data = papa.parse(file, {
+    complete: function(results) {
+        let games = results.data;
+        let chosenGames = [];
+        console.log('complete');
+        for (var i = 0; i < 5; i++) {
+            console.log(i);
+            let chosenGame = games.splice(getRandomInt(games.length - 1), 1);
+            chosenGame.splice(Math.floor(Math.random() * 3) + 1, 1);
+            chosenGame.splice(Math.floor(Math.random() * 2) + 1, 1);
+            chosenGames.push(chosenGame);
+        }
+        console.log(chosenGames);
+    }
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}

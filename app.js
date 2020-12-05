8038ce31bf6ddfdda44403498db9bfc632dbefbe
Connor Shlatz <cshlatz@gmail.com>
2020-12-05 14:28:53 -0500

    awful awful awful

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const papa = require('papaparse');
 
 const file = fs.createReadStream('games.csv');
 
-var data = papa.parse(file, {
-    complete: function(results) {
-        let games = results.data;
-        let chosenGames = [];
-        for (var i = 0; i < 5; i++) {
-            let chosenGame = games.splice(getRandomInt(games.length - 1), 1);
-            chosenGame.splice(Math.floor(Math.random() * 3) + 1, 1);
-            chosenGame.splice(Math.floor(Math.random() * 2) + 1, 1);
-            chosenGames.push(chosenGame);
+export function asb() {
+    var data = papa.parse(file, {
+        complete: function(results) {
+            let games = results.data;
+            let chosenGames = [];
+            for (var i = 0; i < 5; i++) {
+                let chosenGame = games.splice(getRandomInt(games.length - 1), 1);
+                chosenGame.splice(Math.floor(Math.random() * 3) + 1, 1);
+                chosenGame.splice(Math.floor(Math.random() * 2) + 1, 1);
+                chosenGames.push(chosenGame);
+            }
+            return chosenGames;
         }
-        console.log(chosenGames);
-    }
-});
+    });
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));

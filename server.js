6d680e189133df6b409406da8a6656d0b01dc81d
Connor Shlatz <cshlatz@gmail.com>
2020-12-12 11:09:03 -0500

    Really awful code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const papa = require('papaparse');
 const file = fs.readFileSync('games.csv', 'utf8');
+const path = require('path');
 
 var express = require('express');
 var app = express();
@@ -11,25 +12,53 @@ var port = process.env.PORT || 8080;
 
 // set the home page route
 app.get('/', function(req, res) {
-    asb(res);
+    res.sendFile(path.join(__dirname + '/index.html'));    
+});
+
+app.get('/generate', function(req, res) {
+    asb(req, res);
 });
 
 app.listen(port, function() {
 });
 
 
-function asb(res) {
+function asb(req, res) {
     var data = papa.parse(file, {
         complete: function(results) {
             let games = results.data;
             let chosenGames = '';
-            for (var i = 0; i < 5; i++) {
+            let golfObjectiveFlag = false;
+            for (var i = 0; i < req.query.count; i++) {
                 let game = games.splice(getRandomInt(games.length - 1), 1)[0];
                 let thegame = game[0];
-                let objective = game[Math.floor(Math.random() * Math.floor(game.length - 1)) + 1];
-                chosenGames = chosenGames + "<p>GAME: " + thegame + " - " + objective + "</p><br>";
+                let objective;
+                if (req.query.type === "golf") {
+                    if (!golfObjectiveFlag) {
+                        let rng = Math.floor(Math.random() * Math.floor(game.length - 1)) + 1;
+                        if (rng === 1 && i + 1 === req.query.count) {
+                            let rng = Math.floor(Math.random() * Math.floor(game.length)) + 1;
+                        }
+                        objective = game[rng];
+                        if (rng > 1) {
+                            golfObjectiveFlag = true;
+                        }
+                    } else {
+                        objective = game[1];
+                    }
+                    if (i + 1 == req.query.count) {
+                        thegame = '<b>' + thegame + '</b>';
+                    }
+                } else {
+                    objective = "Get a high score";
+                }
+                chosenGames = chosenGames + "<tr scope='row'><td>" + thegame + "</td><td>" + objective + "</td></tr>";
             }
-            res.send(chosenGames);
+            let html = '<table class="table table-striped"><thead><tr><th scope="col">Game</th><th scope="col">Objective</th></tr></thead><tbody>' + chosenGames + '</tbody></table>';
+            if (req.query.type === "golf") {
+                html = html + '<br>Bold game indicates high score tiebreaker';
+            }
+            res.send(html);
         }
     });
 };
@@ -37,3 +66,4 @@ function asb(res) {
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 };
+
